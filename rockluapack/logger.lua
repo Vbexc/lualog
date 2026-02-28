e0284f803bc9f629d2fc84c67d9d3895813f2258
Vbexc <lanekarst1@gmail.com>
2026-02-28 10:34:10 -0800

    Refactor logger.lua to remove unused code

--- rockluapack/logger.lua
+++ rockluapack/logger.lua
@@ -14,32 +14,9 @@ function logger.log(level, message)
 	 local output = string.format("[%s] [%s] %s", timestamp, level, message)
 	 print(output)
 end
-
-local function errorHandler(err)
-	local trace = debug.traceback(err, 2)
-	return trace
-end
-
-local function inner()
-	error("Something went wrong")
-end
-
-local function outer()
-	inner()
-end
-
-local succes, message = xpcall(outer, errorHandler)
-print(message)
-
-local status, result = pcall(divide, 10, 0)
-if status then
-    print("Result:", result)
-else
-    logger.log("error", "Caught division error: " .. result)
-end
-
 	
 return logger
+
 
 
 
